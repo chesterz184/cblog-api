80eacfbd675a8c088e29eaccdf7bcd2a81016032
chesterz184 <chesterz0310@hotmail.com>
2017-04-24 21:43:34 +0800

    remove resource-router-middleware

--- src/api/index.js
+++ src/api/index.js
@@ -1,20 +1,29 @@
 import { version } from '../../package.json';
 import { Router } from 'express';
-import facets from './facets';
-import users from './users';
+// import facets from './facets';
+// import users from './users';
+import User from '../models/users'
 
 export default ({ config, db }) => {
 	let api = Router();
 
 	// mount the facets resource
-	api.use('/facets', facets({ config, db }));
+	// api.use('/facets', facets({ config, db }));
 
-	api.use('/users', users({ config, db }));
+	// api.use('/users', users({ config, db }));
 
 	// perhaps expose some API metadata at the root
 	api.get('/', (req, res) => {
 		res.json({ version });
 	});
+
+	api.get('/user/:id', (req, res) => {
+		User.findById(req.params.id, function(err, user) {
+            if (err)
+                res.send(err);
+            res.json(user);
+        });
+	})
 
 	return api;
 }
